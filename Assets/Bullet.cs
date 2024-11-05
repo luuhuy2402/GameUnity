ddced786ddc5f96d3c4549267f9446815d964790
DoHuong <dthuhuong133@gmail.com>
2024-11-05 23:21:37 +0700

    bullet

---
+++ Assets/Bullet.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Bullet : MonoBehaviour
+{
+    public int n;
+	public Text myText;
+	void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/ShotingItem.cs
+++ Assets/Scripts/ShotingItem.cs
@@ -1,34 +1,57 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ShotingItem : MonoBehaviour
 {
     Animator Animator;
     Object Ball;
+    public int n=5;
+	public Text myText;
 	// Start is called before the first frame update
 	void Start()
     {
 		Ball = Resources.Load("Shotting");
         Animator = GetComponent<Animator>();
-        
-    }
+		UpdateBulletCountUI();
+
+	}
 
     // Update is called once per frame
     void Update()
     {
         
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(1)& n>0)
         {
-            Animator.SetBool("isShoot", true);
-            GameObject fireball=(GameObject)Instantiate(Ball);
-            fireball.transform.position = new Vector3(transform.position.x + .4f, transform.position.y + .2f, -1);
-            Invoke("TrigerAnim", 2f);
+            Shoot();
         }
-       
-    }
+		if (Input.GetKeyDown(KeyCode.H))
+		{
+			n += 2;
+            UpdateBulletCountUI();
+		}
+
+	}
+    void Shoot()
+    {
+		Animator.SetBool("isShoot", true);
+		GameObject fireball = (GameObject)Instantiate(Ball);
+		fireball.transform.position = new Vector3(transform.position.x + .4f, transform.position.y + .2f, -1);
+        n--;
+        UpdateBulletCountUI();
+		Invoke("TrigerAnim", 2f);
+
+	}
     void TrigerAnim()
     {
         Animator.SetBool("isShoot", false);
-    }    
+    }
+	void UpdateBulletCountUI()
+	{
+		if (myText != null)
+		{
+			myText.text = n + "X "  ;
+		}
+	}
 }
